6475df60555509cb7ee0dd1f1edc4d8b39d11ce5
rahmaboukhris <64844430+rahmaboukhris@users.noreply.github.com>
2020-05-07 10:50:23 +0200

    Update PayFeesForm.php

--- PayFeesForm.php
+++ PayFeesForm.php
@@ -21,5 +21,7 @@ class PayFeesForm extends Form
             $validator
                 ->requirePresence("payment[payment_made_by]",true,['message' => 'This value is required '])
                 ->notEmpty('payment.payment_made_by');
+                ->notEmpty('payment.payment_made_by');
+                ->notEmpty('payment.payment_made_by');
     }
 }
